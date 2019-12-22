b2c0e2d9cf8e78e418ff4193f2df0aa978ef812a
Jay <psatdoctor@naver.com>
2019-12-22 11:49:28 +0900

    Layout2
    페이지 주소와 일치할 때 글씨 색 변경

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -14,26 +14,41 @@ const UL = styled.div`
 
 
 
-const Layout = ({children}) => {
+const Layout = ({children, match, history}) => {
+    const isActive = path => {
+        if(match.path === path) {
+            return { color: '#000' };
+        } else {
+            return { color: '#fff' };
+        }
+    };
+    
     const nav = () => (
         //파란선 디자인 포함(부트스트랩 기본 디자인)
         <UL className="nav nav-tabs bg-primary">
             <li className="nav-item">
-                <Link to="/" className="text-light nav-link">HOME</Link>
+                <Link to="/" className="nav-link" style={isActive('/')}>
+                    HOME
+                </Link>
             </li>
             <li className="nav-item">
-                <Link to="/register" className="text-light nav-link">Register</Link>
+                <Link to="/register" className="nav-link" style={isActive('/register')}>
+                    Register
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/login" className="nav-link" style={isActive('/login')}>
+                    Login
+                </Link>
             </li>
         </UL>
     )
     return (
         <Fragment>
             {nav()}
-            <UL className="container">
-                {children}
-            </UL>
+            <div className="container">{children}</div>
         </Fragment>
     )
 }
 
-export default Layout;
+export default withRouter(Layout);
